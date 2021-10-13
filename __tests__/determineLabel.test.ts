0924b39784d024f613ba9439d4a09fb803560f00
ikanago <ikanago-dev@protonmail.com>
2021-10-13 11:29:24 +0900

    Implement determine label

--- __tests__/determineLabel.test.ts
+++ __tests__/determineLabel.test.ts
@@ -2,19 +2,44 @@ import {expect, test} from '@jest/globals';
 import {determineLabel} from '../src/label';
 
 test('outdated', () => {
-    const deadline = new Date(2021, 10, 12, 9, 0, 0);
-    const now = new Date(2021, 10, 13, 9, 0, 0);
+    const deadline = new Date(2021, 10, 1, 9, 0, 0);
+    const now = new Date(2021, 10, 2, 9, 0, 0);
     expect(determineLabel(deadline, now)).toEqual('outdated');
 });
 
 test('2 day before and less than 48 hours difference', () => {
-    const deadline = new Date(2021, 10, 15, 8, 0, 0);
-    const now = new Date(2021, 10, 13, 9, 0, 0);
+    const deadline = new Date(2021, 10, 3, 8, 0, 0);
+    const now = new Date(2021, 10, 1, 9, 0, 0);
     expect(determineLabel(deadline, now)).toEqual({days: 2});
 });
 
 test('2 day before and more than 48 hours difference', () => {
-    const deadline = new Date(2021, 10, 15, 10, 0, 0);
-    const now = new Date(2021, 10, 13, 9, 0, 0);
+    const deadline = new Date(2021, 10, 3, 10, 0, 0);
+    const now = new Date(2021, 10, 1, 9, 0, 0);
     expect(determineLabel(deadline, now)).toEqual({days: 2});
+});
+
+test('Just 1 week before 7 days before', () => {
+    const deadline = new Date(2021, 10, 8, 9, 0, 0);
+    const now = new Date(2021, 10, 1, 9, 0, 0);
+    expect(determineLabel(deadline, now)).toEqual({days: 7});
+});
+
+test.each([
+    [new Date(2021, 10, 13), {weeks: 1}], // Saturday
+    [new Date(2021, 10, 14), {weeks: 2}], // Sunday
+    [new Date(2021, 10, 20), {weeks: 2}], // Saturday
+    [new Date(2021, 10, 21), {weeks: 3}], // Sunday
+])('2 weeks before', (deadline, expected) => {
+    const now = new Date(2021, 10, 1); // Tuesday
+    expect(determineLabel(deadline, now)).toEqual(expected);
+});
+
+test.each([
+    [new Date(2021, 10, 28), {weeks: 4}], // Sunday
+    [new Date(2021, 11, 4), {weeks: 4}], // Saturday
+    [new Date(2021, 11, 5), {months: 1}], // Sunday
+])('4 weeks before', (deadline, expected) => {
+    const now = new Date(2021, 10, 1);
+    expect(determineLabel(deadline, now)).toEqual(expected);
 });
--- src/label.ts
+++ src/label.ts
@@ -1,4 +1,9 @@
-import {differenceInCalendarDays, isAfter} from 'date-fns';
+import {
+    differenceInCalendarDays,
+    differenceInCalendarMonths,
+    differenceInCalendarWeeks,
+    isAfter,
+} from 'date-fns';
 
 type DeadlineLabel = 'outdated' | DaysBefore | WeeksBefore | MonthsBefore;
 
@@ -14,17 +19,28 @@ type MonthsBefore = {
     months: number;
 };
 
+/**
+ * Determines label type based on deadline and current time.
+ * @param {Date} deadline
+ * @param {Date} now
+ */
 const determineLabel = (deadline: Date, now: Date): DeadlineLabel => {
     if (isAfter(now, deadline)) {
         return 'outdated';
     }
 
     const daysBefore = differenceInCalendarDays(deadline, now);
-    if (daysBefore < 7) {
+    if (daysBefore <= 7) {
         return {days: daysBefore};
     }
 
-    return {months: 0};
+    const weeksBefore = differenceInCalendarWeeks(deadline, now);
+    if (weeksBefore <= 4) {
+        return {weeks: weeksBefore};
+    }
+
+    const monthsBefore = differenceInCalendarMonths(deadline, now);
+    return {months: monthsBefore};
 };
 
 export {determineLabel};
