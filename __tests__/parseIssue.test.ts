e16627d02e7a0c9c0acaeb6a433c79cbb98ab911
ikanago <ikanago-dev@protonmail.com>
2021-10-15 22:47:00 +0900

    Parse issue comment with command

---
+++ __tests__/parseIssue.test.ts
@@ -0,0 +1,66 @@
+import {parseIssueBodyOrComment} from '../src/label';
+import {expect, test} from '@jest/globals';
+
+test('/deadline yyyy/MM/dd HH:mm', () => {
+    const comment = `Homework
+    /deadline 2021/12/01  09:12`;
+    expect(parseIssueBodyOrComment(comment)).toEqual(
+        new Date(2021, 11, 1, 9, 12)
+    );
+});
+
+test('/deadline yyyy/MM/dd', () => {
+    const comment = `Homework
+    /deadline 2021/12/01`;
+    expect(parseIssueBodyOrComment(comment)).toEqual(
+        new Date(2021, 11, 1)
+    );
+});
+
+test('/deadline MM/dd', () => {
+    const comment = `Homework
+    /deadline 2021/12/01`;
+    expect(parseIssueBodyOrComment(comment)).toEqual(
+        new Date(2021, 11, 1)
+    );
+});
+
+test('/deadline MM/dd', () => {
+    const comment = `Homework
+    /deadline 2021/12/01`;
+    expect(parseIssueBodyOrComment(comment)).toEqual(
+        new Date(2021, 11, 1)
+    );
+});
+
+test('/deadline M/d', () => {
+    const comment = `Homework
+    /deadline 2021/3/1`;
+    expect(parseIssueBodyOrComment(comment)).toEqual(
+        new Date(2021, 2, 1)
+    );
+});
+
+test('/deadline yyyy/MM/dd H:m', () => {
+    const comment = `Homework
+    /deadline 2021/12/01 9:3`;
+    expect(parseIssueBodyOrComment(comment)).toEqual(
+        new Date(2021, 11, 1, 9, 3)
+    );
+});
+
+test('Comment does not start with command', () => {
+    const comment = `Blah blah /deadline 2021/12/01 09:12`;
+    expect(parseIssueBodyOrComment(comment)).toEqual(
+        new Date(2021, 11, 1, 9, 12)
+    );
+});
+
+test('Multiple commands', () => {
+    const comment = `Blah blah /deadline 2021/12/01 09:12
+    /deadline 2021/11/1
+    /deadline 2021/1/1`;
+    expect(parseIssueBodyOrComment(comment)).toEqual(
+        new Date(2021, 0, 1)
+    );
+});
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 
 async function run(): Promise<void> {
     try {
-        core.debug("Hello");
+        core.debug('Hello');
     } catch (error) {
         if (error instanceof Error) {
             core.setFailed(error.message);
