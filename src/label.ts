5d46c946075c7488d23abefd51e70ff2e9c3e1d8
ikanago <ikanago-dev@protonmail.com>
2021-10-20 11:25:29 +0900

    Adjust time zone of deadline
    Time zone of deadline and CI machine will be different, so adjust the deadline's one.
    Deadline: convert to UTC time by `zonedTimeToUtc` with specified time zone
    CI machine: just get time with `new Date()` which is UTC time

--- src/label.ts
+++ src/label.ts
@@ -4,6 +4,7 @@ import {
     differenceInCalendarWeeks,
     isAfter,
 } from 'date-fns';
+import {zonedTimeToUtc} from 'date-fns-tz';
 
 type DeadlineLabel = 'outdated' | DaysBefore | WeeksBefore | MonthsBefore;
 
@@ -23,24 +24,36 @@ type MonthsBefore = {
  * Determines label type based on deadline and current time.
  * @param {Date} deadline
  * @param {Date} now
+ * @param {string} timeZone Time zone of the `deadline`. An offset from UTC or IANA time zone.
+ * @returns {DeadlineLabel} The kind of label to apply.
  */
-const determineLabel = (deadline: Date, now: Date): DeadlineLabel => {
-    if (isAfter(now, deadline)) {
+const determineLabel = (
+    deadline: Date,
+    now: Date,
+    timeZone: string = 'UTC'
+): DeadlineLabel => {
+    const adjustedDeadline = adjustDeadlineTimezone(deadline, timeZone);
+
+    if (isAfter(now, adjustedDeadline)) {
         return 'outdated';
     }
 
-    const daysBefore = differenceInCalendarDays(deadline, now);
+    const daysBefore = differenceInCalendarDays(adjustedDeadline, now);
     if (daysBefore <= 7) {
         return {days: daysBefore};
     }
 
-    const weeksBefore = differenceInCalendarWeeks(deadline, now);
+    const weeksBefore = differenceInCalendarWeeks(adjustedDeadline, now);
     if (weeksBefore <= 4) {
         return {weeks: weeksBefore};
     }
 
-    const monthsBefore = differenceInCalendarMonths(deadline, now);
+    const monthsBefore = differenceInCalendarMonths(adjustedDeadline, now);
     return {months: monthsBefore};
+};
+
+const adjustDeadlineTimezone = (deadline: Date, timeZone: string): Date => {
+    return zonedTimeToUtc(deadline, timeZone);
 };
 
 export {determineLabel};
