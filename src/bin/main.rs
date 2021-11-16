7f3584e9b0fbd54a32613ae01c41e4af0336b7cc
ikanago <ikanago-dev@protonmail.com>
2021-11-16 10:53:38 +0900

    Skip first element of command line args; it's binary name

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,6 +5,7 @@ async fn main() {
     // Don't use clap or structopt here; arguments presence and order is stable.
     // They are guranteed by metadata of GitHub Actions.
     let mut args = std::env::args();
+    let _bin_name = args.next().unwrap();
     let token = args.next().unwrap();
     let repository = args.next().unwrap();
     let config = Config::new(repository);
